ca5ae9370df4a714ad9091a0a922cb4752a0d7eb
rmichura <r.michura@wp.pl>
2022-03-28 14:07:35 +0200

    Add correct src database

--- app/config.js
+++ app/config.js
@@ -2,5 +2,5 @@ require('dotenv').config()
 
 module.exports = {
     port: process.env.PORT || 3000,
-    database: process.env.DATABASE || 'mongodb+srv://user:user@todo.ahtiv.mongodb.net/ToDo?retryWrites=true&w=majority'
+    database: process.env.DATABASE || 'mongodb+srv://user:user@todo.cl3yc.mongodb.net/ToDo?retryWrites=true&w=majority'
 }
