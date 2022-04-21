b032146caaf74dbde0df08dbe5dca23e0db60c01
rmichura <r.michura@wp.pl>
2022-04-21 13:58:46 +0200

    Added home page in frontend

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-container class="margin-top">
+    <v-row class="justify-center">
+      <span>Welcome to the ToDo app</span>
+    </v-row>
+    <v-row class="justify-center">
+      <v-btn
+        color="primary"
+        elevation="7"
+        large
+        @click="toSignIn"
+      >
+        Get started
+        <v-icon
+          class="ma-1"
+      >
+          mdi-arrow-right
+        </v-icon></v-btn>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import router from "@/router";
+
+export default {
+  name: "Main",
+
+  methods: {
+    toSignIn() {
+      router.push('/login')
+    },
+  }
+}
+</script>
+
+<style scoped>
+span {
+  font-size: 50px;
+  margin-top: 5em;
+  margin-bottom: 1em;
+  /*color: rgb(25, 115, 205);*/
+}
+</style>
