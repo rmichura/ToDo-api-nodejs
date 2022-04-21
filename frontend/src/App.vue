ae6c1665c2074344936a7ff4d6e7e92699c50457
rmichura <r.michura@wp.pl>
2022-04-21 13:56:41 +0200

    Added configuration files in frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <navigator-bar/>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+import NavigatorBar from "@/components/NavigatorBar";
+
+export default {
+  name: 'App',
+  components: {NavigatorBar},
+  data: () => ({
+    //
+  }),
+}
+</script>
